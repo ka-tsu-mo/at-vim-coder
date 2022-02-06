897079d2063ac7f62a8d7b90ad28e999b9b2457b
ka-tsu-mo <katsumo.github@gmail.com>
2022-02-06 15:39:53 +0900

    use spaces instead of tabs for indentation

--- python3/at_vim_coder.py
+++ python3/at_vim_coder.py
@@ -12,254 +12,254 @@ AT_CODER_BASE_URL = 'https://atcoder.jp'
 AT_CODER_LOGIN_URL = AT_CODER_BASE_URL + '/login'
 
 class AtVimCoder:
-	def __init__(self):
-		self._session = requests.Session()
-		self._cookies_path = os.path.join(vim.eval('g:at_vim_coder_repo_dir'), 'cookies')
-		self._locale = vim.eval('$LANG')
-		self._tex_handler = tex_handler.AVCTexHandler()
-		if os.path.exists(self._cookies_path):
-			with open(self._cookies_path, 'rb') as f:
-				self._session.cookies.update(pickle.load(f))
+    def __init__(self):
+        self._session = requests.Session()
+        self._cookies_path = os.path.join(vim.eval('g:at_vim_coder_repo_dir'), 'cookies')
+        self._locale = vim.eval('$LANG')
+        self._tex_handler = tex_handler.AVCTexHandler()
+        if os.path.exists(self._cookies_path):
+            with open(self._cookies_path, 'rb') as f:
+                self._session.cookies.update(pickle.load(f))
 
-	def get_cookies(self):
-		cookies = self._session.cookies.get_dict()
-		vim.command(f'let l:cookies = {cookies}')
+    def get_cookies(self):
+        cookies = self._session.cookies.get_dict()
+        vim.command(f'let l:cookies = {cookies}')
 
-	def _get_csrf_token(self):
-		try:
-			response = self._session.get(AT_CODER_LOGIN_URL, timeout=3.0)
-		except (ConnectionError, HTTPError, Timeout):
-			raise
-		else:
-			bs_get_resp = BeautifulSoup(response.text, 'html.parser')
-			return bs_get_resp.find(attrs={'name': 'csrf_token'}).get('value')
-
-
-	def _save_cookies(self):
-		with open(self._cookies_path, 'wb') as f:
-			pickle.dump(self._session.cookies, f)
-
-	def delete_cookies(self):
-		self._session.cookies.clear()
-		os.remove(self._cookies_path)
-
-	def logout(self):
-		try:
-			csrf_token = self._get_csrf_token()
-			logout_data = {
-				'csrf_token': csrf_token
-			}
-			response = self._session.post(f'{AT_CODER_BASE_URL}/logout', data=logout_data, allow_redirects=False, timeout=3.0)
-		except (ConnectionError, HTTPError, Timeout) as e:
-			e_str = str(e)
-			vim.command(f'let err = "{e_str}"')
-		else:
-			if response.status_code == 302:
-				vim.command('let logout_success = 1')
-			else:
-				vim.command('let logout_success = 0')
+    def _get_csrf_token(self):
+        try:
+            response = self._session.get(AT_CODER_LOGIN_URL, timeout=3.0)
+        except (ConnectionError, HTTPError, Timeout):
+            raise
+        else:
+            bs_get_resp = BeautifulSoup(response.text, 'html.parser')
+            return bs_get_resp.find(attrs={'name': 'csrf_token'}).get('value')
 
 
+    def _save_cookies(self):
+        with open(self._cookies_path, 'wb') as f:
+            pickle.dump(self._session.cookies, f)
 
-	def login(self, name, password, save_cookies):
-		try:
-			csrf_token = self._get_csrf_token()
-			login_data = {
-				'csrf_token': csrf_token,
-				'username': name,
-				'password': password
-			}
-			login_result = self._session.post(AT_CODER_LOGIN_URL, data=login_data, timeout=3.0)
-			bs_post_resp = BeautifulSoup(login_result.text, 'html.parser')
-		except (ConnectionError, HTTPError, Timeout) as e:
-			e_str = str(e)
-			vim.command(f'let err = "{e_str}"')
-		else:
-			if bs_post_resp.find(attrs={'class': 'alert-success'}):
-				if save_cookies:
-					self._save_cookies()
-				vim.command('let l:login_success = 1')
-			else:
-				vim.command('let l:login_success = 0')
+    def delete_cookies(self):
+        self._session.cookies.clear()
+        os.remove(self._cookies_path)
 
-
-	def check_login(self):
-		url = AT_CODER_BASE_URL + '/settings'
-		try:
-			response = self._session.get(url, allow_redirects=False, timeout=3.0)
-		except (ConnectionError, HTTPError, Timeout) as e:
-			e_str = str(e)
-			vim.command(f'let err = "{e_str}"')
-		else:
-			if response.status_code == 302:
-				vim.command('let l:logged_in = 0')
-			else:
-				vim.command('let l:logged_in = 1')
+    def logout(self):
+        try:
+            csrf_token = self._get_csrf_token()
+            logout_data = {
+                'csrf_token': csrf_token
+            }
+            response = self._session.post(f'{AT_CODER_BASE_URL}/logout', data=logout_data, allow_redirects=False, timeout=3.0)
+        except (ConnectionError, HTTPError, Timeout) as e:
+            e_str = str(e)
+            vim.command(f'let err = "{e_str}"')
+        else:
+            if response.status_code == 302:
+                vim.command('let logout_success = 1')
+            else:
+                vim.command('let logout_success = 0')
 
 
-	def create_task_list(self, contest_id):
-		try:
-			bs_contest_resp = self._download_task_list(contest_id)
-		except (ConnectionError, HTTPError, Timeout) as e:
-			e_str = str(e)
-			vim.command(f'let err = "{e_str}"')
-		else:
-			if bs_contest_resp is None:
-				vim.command('let l:created_task_list = {}')
-			else:
-				task_table = bs_contest_resp.tbody.find_all('tr')
-				task_list = {}
-				for task in task_table:
-					task_list_info = task.find_all('td')
-					task_id = task_list_info[0].text
-					task_title = task_list_info[1].text
-					task_url = task_list_info[1].a.get("href")
-					task_list[task_id] = { 'task_title': task_title, 'task_url': task_url }
-				vim.command(f'let l:created_task_list= {task_list}')
 
-	def _download_task_list(self, contest_id):
-		url = AT_CODER_BASE_URL + '/contests/' + contest_id + '/tasks'
-		try:
-			response = self._session.get(url, timeout=3.0)
-		except (ConnectionError, HTTPError, Timeout):
-			raise
-		else:
-			if response.status_code == 404:
-				return None
-			else:
-				return BeautifulSoup(response.text, 'html.parser')
+    def login(self, name, password, save_cookies):
+        try:
+            csrf_token = self._get_csrf_token()
+            login_data = {
+                'csrf_token': csrf_token,
+                'username': name,
+                'password': password
+            }
+            login_result = self._session.post(AT_CODER_LOGIN_URL, data=login_data, timeout=3.0)
+            bs_post_resp = BeautifulSoup(login_result.text, 'html.parser')
+        except (ConnectionError, HTTPError, Timeout) as e:
+            e_str = str(e)
+            vim.command(f'let err = "{e_str}"')
+        else:
+            if bs_post_resp.find(attrs={'class': 'alert-success'}):
+                if save_cookies:
+                    self._save_cookies()
+                vim.command('let l:login_success = 1')
+            else:
+                vim.command('let l:login_success = 0')
 
-	def _get_section_title_by_language(self):
-		section_titles = {}
-		if self._locale[:2] == 'ja':
-			section_titles['sample_input'] = '入力例'
-			section_titles['sample_output'] = '出力例'
-		else:
-			section_titles['sample_input'] = 'Sample Input'
-			section_titles['sample_output'] = 'Sample Output'
-		return section_titles
 
-	def create_task_info(self, url):
-		try:
-			sections = self._download_task(url)
-		except (ConnectionError, HTTPError, Timeout) as e:
-			e_str = str(e)
-			vim.command(f'let err = "{e_str}"')
-		else:
-			task_info = {}
-			problem_info = []
-			sample_input = []
-			sample_output = []
-			section_titles = self._get_section_title_by_language()
-			for section in sections:
-				if section.h3 is not None:
-					title = section.h3.text
-				else:
-					break
-				if title.startswith(section_titles['sample_input']):
-					index = re.search(r'\d+', title).group(0)
-					section.h3.decompose()
-					sample_input.insert(int(index), self._create_sample_io(section, False))
-				elif title.startswith(section_titles['sample_output']):
-					index = re.search(r'\d+', title).group(0)
-					section.h3.decompose()
-					sample_output.insert(int(index), self._create_sample_io(section, True))
-				else:
-					problem_info.append('['+section.h3.text+']')
-					section.h3.decompose()
-					self._tex_handler.replace_var_text(section)
-					self._add_single_quote_to_code_tag(section)
-					lines = [line.strip() for line in section.text.splitlines() if line]
-					problem_info.extend(lines)
-			task_info['problem_info'] = problem_info
-			task_info['sample_input'] = sample_input
-			task_info['sample_output'] = sample_output
-			vim.command(f'let l:task_info = {task_info}')
+    def check_login(self):
+        url = AT_CODER_BASE_URL + '/settings'
+        try:
+            response = self._session.get(url, allow_redirects=False, timeout=3.0)
+        except (ConnectionError, HTTPError, Timeout) as e:
+            e_str = str(e)
+            vim.command(f'let err = "{e_str}"')
+        else:
+            if response.status_code == 302:
+                vim.command('let l:logged_in = 0')
+            else:
+                vim.command('let l:logged_in = 1')
 
-	def _download_task(self, url):
-		url = AT_CODER_BASE_URL + url
-		try:
-			response = self._session.get(url, timeout=3.0)
-		except (ConnectionError, HTTPError, Timeout):
-			raise
-		else:
-			bs_task_soup = BeautifulSoup(response.text, 'html.parser')
-			if self._locale[:2] == 'ja':
-				span = bs_task_soup.find('span', attrs={'class': 'lang-ja'})
-			else:
-				span = bs_task_soup.find('span', attrs={'class': 'lang-en'})
-			if span is None:
-				return bs_task_soup.find_all('section')
-			else:
-				return span.find_all('section')
 
-	def _add_single_quote_to_code_tag(self, section):
-		code_tags = section.find_all('code')
-		for code_tag in code_tags:
-			current_text = code_tag.text
-			code_tag.string = '\'' + current_text + '\''
+    def create_task_list(self, contest_id):
+        try:
+            bs_contest_resp = self._download_task_list(contest_id)
+        except (ConnectionError, HTTPError, Timeout) as e:
+            e_str = str(e)
+            vim.command(f'let err = "{e_str}"')
+        else:
+            if bs_contest_resp is None:
+                vim.command('let l:created_task_list = {}')
+            else:
+                task_table = bs_contest_resp.tbody.find_all('tr')
+                task_list = {}
+                for task in task_table:
+                    task_list_info = task.find_all('td')
+                    task_id = task_list_info[0].text
+                    task_title = task_list_info[1].text
+                    task_url = task_list_info[1].a.get("href")
+                    task_list[task_id] = { 'task_title': task_title, 'task_url': task_url }
+                vim.command(f'let l:created_task_list= {task_list}')
 
-	def _create_sample_io(self, section, is_out):
-		pre_tag = section.find('pre').contents[0]
-		section.pre.decompose()
-		if is_out:
-			sample_output = {}
-			sample_output['value'] = [line for line in pre_tag.splitlines() if line]
-			self._tex_handler.replace_var_text(section)
-			self._add_single_quote_to_code_tag(section)
-			sample_output['explanation'] = [line for line in section.text.splitlines() if line]
-			return sample_output
-		else:
-			return [line for line in pre_tag.splitlines() if line]
+    def _download_task_list(self, contest_id):
+        url = AT_CODER_BASE_URL + '/contests/' + contest_id + '/tasks'
+        try:
+            response = self._session.get(url, timeout=3.0)
+        except (ConnectionError, HTTPError, Timeout):
+            raise
+        else:
+            if response.status_code == 404:
+                return None
+            else:
+                return BeautifulSoup(response.text, 'html.parser')
 
-	def get_latest_submission(self, contest_id, task_screen_name):
-		try:
-			tbody = self._download_submissions_list(contest_id, task_screen_name)
-		except (ConnectionError, HTTPError, Timeout) as e:
-			e_str = str(e)
-			vim.command(f'let err = "{e_str}"')
-		else:
-			td = tbody.tr.find_all('td')
-			submission = {
-				'time': td[0].text,
-				'language': td[3].text,
-				'status': td[6].text
-			}
-			vim.command(f'let latest_submission = {submission}')
+    def _get_section_title_by_language(self):
+        section_titles = {}
+        if self._locale[:2] == 'ja':
+            section_titles['sample_input'] = '入力例'
+            section_titles['sample_output'] = '出力例'
+        else:
+            section_titles['sample_input'] = 'Sample Input'
+            section_titles['sample_output'] = 'Sample Output'
+        return section_titles
 
-	def create_submissions_list(self, contest_id, task_screen_name):
-		try:
-			tbody = self._download_submissions_list(contest_id, task_screen_name)
-		except (ConnectionError, HTTPError, Timeout) as e:
-			e_str = str(e)
-			vim.command(f'let err = "{e_str}"')
-		else:
-			if tbody is None:
-				vim.command('let submissions_list = []')
-				return
-			submissions_table = tbody.find_all('tr')
-			submissions_list = []
-			for tr in submissions_table:
-				td = tr.find_all('td')
-				submission = {
-					'time': td[0].text,
-					'language': td[3].text,
-					'status': td[6].text
-				}
-				submissions_list.append(submission)
-			submissions_list = sorted(submissions_list, key=lambda x: x['time'])
-			vim.command(f'let submissions_list = {submissions_list}')
+    def create_task_info(self, url):
+        try:
+            sections = self._download_task(url)
+        except (ConnectionError, HTTPError, Timeout) as e:
+            e_str = str(e)
+            vim.command(f'let err = "{e_str}"')
+        else:
+            task_info = {}
+            problem_info = []
+            sample_input = []
+            sample_output = []
+            section_titles = self._get_section_title_by_language()
+            for section in sections:
+                if section.h3 is not None:
+                    title = section.h3.text
+                else:
+                    break
+                if title.startswith(section_titles['sample_input']):
+                    index = re.search(r'\d+', title).group(0)
+                    section.h3.decompose()
+                    sample_input.insert(int(index), self._create_sample_io(section, False))
+                elif title.startswith(section_titles['sample_output']):
+                    index = re.search(r'\d+', title).group(0)
+                    section.h3.decompose()
+                    sample_output.insert(int(index), self._create_sample_io(section, True))
+                else:
+                    problem_info.append('['+section.h3.text+']')
+                    section.h3.decompose()
+                    self._tex_handler.replace_var_text(section)
+                    self._add_single_quote_to_code_tag(section)
+                    lines = [line.strip() for line in section.text.splitlines() if line]
+                    problem_info.extend(lines)
+            task_info['problem_info'] = problem_info
+            task_info['sample_input'] = sample_input
+            task_info['sample_output'] = sample_output
+            vim.command(f'let l:task_info = {task_info}')
 
-	def _download_submissions_list(self, contest_id, task_screen_name):
-		url = f'{AT_CODER_BASE_URL}/contests/{contest_id}/submissions/me?f.Task={task_screen_name}'
-		try:
-			response = self._session.get(url, timeout=3.0)
-		except (ConnectionError, HTTPError, Timeout):
-			raise
-		else:
-			bs_submissions_resp = BeautifulSoup(response.text, 'html.parser')
-			if bs_submissions_resp.tbody is None:
-				return None
-			else:
-				return bs_submissions_resp.tbody
+    def _download_task(self, url):
+        url = AT_CODER_BASE_URL + url
+        try:
+            response = self._session.get(url, timeout=3.0)
+        except (ConnectionError, HTTPError, Timeout):
+            raise
+        else:
+            bs_task_soup = BeautifulSoup(response.text, 'html.parser')
+            if self._locale[:2] == 'ja':
+                span = bs_task_soup.find('span', attrs={'class': 'lang-ja'})
+            else:
+                span = bs_task_soup.find('span', attrs={'class': 'lang-en'})
+            if span is None:
+                return bs_task_soup.find_all('section')
+            else:
+                return span.find_all('section')
+
+    def _add_single_quote_to_code_tag(self, section):
+        code_tags = section.find_all('code')
+        for code_tag in code_tags:
+            current_text = code_tag.text
+            code_tag.string = '\'' + current_text + '\''
+
+    def _create_sample_io(self, section, is_out):
+        pre_tag = section.find('pre').contents[0]
+        section.pre.decompose()
+        if is_out:
+            sample_output = {}
+            sample_output['value'] = [line for line in pre_tag.splitlines() if line]
+            self._tex_handler.replace_var_text(section)
+            self._add_single_quote_to_code_tag(section)
+            sample_output['explanation'] = [line for line in section.text.splitlines() if line]
+            return sample_output
+        else:
+            return [line for line in pre_tag.splitlines() if line]
+
+    def get_latest_submission(self, contest_id, task_screen_name):
+        try:
+            tbody = self._download_submissions_list(contest_id, task_screen_name)
+        except (ConnectionError, HTTPError, Timeout) as e:
+            e_str = str(e)
+            vim.command(f'let err = "{e_str}"')
+        else:
+            td = tbody.tr.find_all('td')
+            submission = {
+                'time': td[0].text,
+                'language': td[3].text,
+                'status': td[6].text
+            }
+            vim.command(f'let latest_submission = {submission}')
+
+    def create_submissions_list(self, contest_id, task_screen_name):
+        try:
+            tbody = self._download_submissions_list(contest_id, task_screen_name)
+        except (ConnectionError, HTTPError, Timeout) as e:
+            e_str = str(e)
+            vim.command(f'let err = "{e_str}"')
+        else:
+            if tbody is None:
+                vim.command('let submissions_list = []')
+                return
+            submissions_table = tbody.find_all('tr')
+            submissions_list = []
+            for tr in submissions_table:
+                td = tr.find_all('td')
+                submission = {
+                    'time': td[0].text,
+                    'language': td[3].text,
+                    'status': td[6].text
+                }
+                submissions_list.append(submission)
+            submissions_list = sorted(submissions_list, key=lambda x: x['time'])
+            vim.command(f'let submissions_list = {submissions_list}')
+
+    def _download_submissions_list(self, contest_id, task_screen_name):
+        url = f'{AT_CODER_BASE_URL}/contests/{contest_id}/submissions/me?f.Task={task_screen_name}'
+        try:
+            response = self._session.get(url, timeout=3.0)
+        except (ConnectionError, HTTPError, Timeout):
+            raise
+        else:
+            bs_submissions_resp = BeautifulSoup(response.text, 'html.parser')
+            if bs_submissions_resp.tbody is None:
+                return None
+            else:
+                return bs_submissions_resp.tbody
 
--- python3/tex_handler.py
+++ python3/tex_handler.py
@@ -1,100 +1,100 @@
 import re
 
 class AVCTexHandler:
-	def __init__(self):
-		self._conv_table = [
-				(re.compile(r'\\frac\{.+\}'), self._frac_to_slash),
-				(re.compile(r'_\{[0-9aeijoruvx+-=()]+\}|_[0-9aeijoruvx+-=()]'), self._underscore_to_subscript),
-				(re.compile(r'\^\{[0-9aeijoruvx+-=()]+\}|\^[0-9aeijoruvx+-=()]'), self._caret_to_superscript),
-				(re.compile(r'\\mbox\{.+?\}'), self._mbox_to_text),
-				(re.compile(r'\\sqrt'), '√'),
-				(re.compile(r'\\pm'), '±'),
-				(re.compile(r'\\div'), '÷'),
-				(re.compile(r'\\times'), '×'),
-				(re.compile(r'\\neq'), '≠'),
-				(re.compile(r'\\not='), '≠'),
-				(re.compile(r'\\leqq'), '≦'),
-				(re.compile(r'\\leq'), '≤'),
-				(re.compile(r'\\geqq'), '≧'),
-				(re.compile(r'\\geq'), '≥'),
-				(re.compile(r'\\cap'), '∩'),
-				(re.compile(r'\\cup'), '∪'),
-				(re.compile(r'\\subseteq'), '⊆'),
-				(re.compile(r'\\subset'), '⊂'),
-				(re.compile(r'\\supseteq'), '⊇'),
-				(re.compile(r'\\supset'), '⊃'),
-				(re.compile(r'\\in'), '∈'),
-				(re.compile(r'\\ni'), '∋'),
-				(re.compile(r'\\vee'), '∨'),
-				(re.compile(r'\\\wedge'), '∧'),
-				(re.compile(r'\\cdots'), '⋯'),
-				(re.compile(r'\\ldots'), '…'),
-				(re.compile(r'\\vdots'), '⋮'),
-				(re.compile(r'\\cdot'), '∙'),
-				(re.compile(r'\\bullet'), '⦁'),
-				(re.compile(r'\\left'), ''),
-				(re.compile(r'\\right'), ''),
-				(re.compile(r'\\max'), 'max'),
-				(re.compile(r'\\min'), 'min'),
-				(re.compile(r'\\{'), '{'),
-				(re.compile(r'\\}'), '}'),
-				(re.compile(r'\\ '), '')
-				]
-		self._subscript_table = str.maketrans('aeijoruvx0123456789+-=()', 'ₐₑᵢⱼₒᵣᵤᵥₓ₀₁₂₃₄₅₆₇₈₉₊₋₌₍₎', '_{}')
-		self._superscript_table = str.maketrans('AaBbDdEeGgHhIiJjKkLlMmNnOoPpRrTtUuVvWwcfsxyz0123456789+-=()', 'ᴬᵃᴮᵇᴰᵈᴱᵉᴳᵍᴴʰᴵⁱᴶʲᴷᵏᴸˡᴹᵐᴺⁿᴼᵒᴾᵖᴿʳᵀᵗᵁᵘⱽᵛᵂʷᶜᶠˢˣʸᶻ⁰¹²³⁴⁵⁶⁷⁸⁹⁺⁻⁼⁽⁾', '^{}')
-		self._exclude_pattern = re.compile(r'^[a-zA-Z0-9(...)\s,+=-]+$')
+    def __init__(self):
+        self._conv_table = [
+                (re.compile(r'\\frac\{.+\}'), self._frac_to_slash),
+                (re.compile(r'_\{[0-9aeijoruvx+-=()]+\}|_[0-9aeijoruvx+-=()]'), self._underscore_to_subscript),
+                (re.compile(r'\^\{[0-9aeijoruvx+-=()]+\}|\^[0-9aeijoruvx+-=()]'), self._caret_to_superscript),
+                (re.compile(r'\\mbox\{.+?\}'), self._mbox_to_text),
+                (re.compile(r'\\sqrt'), '√'),
+                (re.compile(r'\\pm'), '±'),
+                (re.compile(r'\\div'), '÷'),
+                (re.compile(r'\\times'), '×'),
+                (re.compile(r'\\neq'), '≠'),
+                (re.compile(r'\\not='), '≠'),
+                (re.compile(r'\\leqq'), '≦'),
+                (re.compile(r'\\leq'), '≤'),
+                (re.compile(r'\\geqq'), '≧'),
+                (re.compile(r'\\geq'), '≥'),
+                (re.compile(r'\\cap'), '∩'),
+                (re.compile(r'\\cup'), '∪'),
+                (re.compile(r'\\subseteq'), '⊆'),
+                (re.compile(r'\\subset'), '⊂'),
+                (re.compile(r'\\supseteq'), '⊇'),
+                (re.compile(r'\\supset'), '⊃'),
+                (re.compile(r'\\in'), '∈'),
+                (re.compile(r'\\ni'), '∋'),
+                (re.compile(r'\\vee'), '∨'),
+                (re.compile(r'\\\wedge'), '∧'),
+                (re.compile(r'\\cdots'), '⋯'),
+                (re.compile(r'\\ldots'), '…'),
+                (re.compile(r'\\vdots'), '⋮'),
+                (re.compile(r'\\cdot'), '∙'),
+                (re.compile(r'\\bullet'), '⦁'),
+                (re.compile(r'\\left'), ''),
+                (re.compile(r'\\right'), ''),
+                (re.compile(r'\\max'), 'max'),
+                (re.compile(r'\\min'), 'min'),
+                (re.compile(r'\\{'), '{'),
+                (re.compile(r'\\}'), '}'),
+                (re.compile(r'\\ '), '')
+                ]
+        self._subscript_table = str.maketrans('aeijoruvx0123456789+-=()', 'ₐₑᵢⱼₒᵣᵤᵥₓ₀₁₂₃₄₅₆₇₈₉₊₋₌₍₎', '_{}')
+        self._superscript_table = str.maketrans('AaBbDdEeGgHhIiJjKkLlMmNnOoPpRrTtUuVvWwcfsxyz0123456789+-=()', 'ᴬᵃᴮᵇᴰᵈᴱᵉᴳᵍᴴʰᴵⁱᴶʲᴷᵏᴸˡᴹᵐᴺⁿᴼᵒᴾᵖᴿʳᵀᵗᵁᵘⱽᵛᵂʷᶜᶠˢˣʸᶻ⁰¹²³⁴⁵⁶⁷⁸⁹⁺⁻⁼⁽⁾', '^{}')
+        self._exclude_pattern = re.compile(r'^[a-zA-Z0-9(...)\s,+=-]+$')
 
-	def replace_var_text(self, section):
-		var_tags = section.find_all('var')
-		for var_tag in var_tags:
-			if var_tag != [] and not self._exclude_pattern.match(var_tag.text):
-				for tex_expression in self._conv_table:
-					if tex_expression[0].search(var_tag.text):
-						var_tag.string = tex_expression[0].sub(tex_expression[1], var_tag.text)
+    def replace_var_text(self, section):
+        var_tags = section.find_all('var')
+        for var_tag in var_tags:
+            if var_tag != [] and not self._exclude_pattern.match(var_tag.text):
+                for tex_expression in self._conv_table:
+                    if tex_expression[0].search(var_tag.text):
+                        var_tag.string = tex_expression[0].sub(tex_expression[1], var_tag.text)
 
-	def _frac_to_slash(self, matchobj):
-		text = matchobj.group(0)
-		table = str.maketrans('{}', '()')
-		for i in range(2):
-			stack = []
-			for j in range(len(text)):
-				if text[j] == '{' or text[j] == '}':
-					stack.append(j)
-				if text[j] == '}':
-					if len(stack) == 2 and text[stack[0]] == '{' and text[stack[1]] == '}':
-						break
-					try:
-						stack.pop()
-						stack.pop()
-					except:
-						return matchobj.group(0)
-			if i == 0:
-				try:
-					if len(text[stack[0]:stack[1]+1]) == 3:
-						numerator = text[stack[0]+1]
-					else:
-						numerator = text[stack[0]:stack[1]+1].translate(table)
-					text = text[stack[1]+1:]
-				except:
-					return matchobj.group(0)
-			if i == 1:
-				try:
-					if len(text[stack[0]:stack[1]+1]) == 3:
-						denominator = text[stack[0]+1]
-					else:
-						denominator = text[stack[0]:stack[1]+1].translate(table)
-					return numerator + '/' + denominator + text[stack[1]+1:]
-				except:
-					return matchobj.group(0)
+    def _frac_to_slash(self, matchobj):
+        text = matchobj.group(0)
+        table = str.maketrans('{}', '()')
+        for i in range(2):
+            stack = []
+            for j in range(len(text)):
+                if text[j] == '{' or text[j] == '}':
+                    stack.append(j)
+                if text[j] == '}':
+                    if len(stack) == 2 and text[stack[0]] == '{' and text[stack[1]] == '}':
+                        break
+                    try:
+                        stack.pop()
+                        stack.pop()
+                    except:
+                        return matchobj.group(0)
+            if i == 0:
+                try:
+                    if len(text[stack[0]:stack[1]+1]) == 3:
+                        numerator = text[stack[0]+1]
+                    else:
+                        numerator = text[stack[0]:stack[1]+1].translate(table)
+                    text = text[stack[1]+1:]
+                except:
+                    return matchobj.group(0)
+            if i == 1:
+                try:
+                    if len(text[stack[0]:stack[1]+1]) == 3:
+                        denominator = text[stack[0]+1]
+                    else:
+                        denominator = text[stack[0]:stack[1]+1].translate(table)
+                    return numerator + '/' + denominator + text[stack[1]+1:]
+                except:
+                    return matchobj.group(0)
 
-	def _underscore_to_subscript(self, matchobj):
-		original_text = matchobj.group(0)
-		return original_text.translate(self._subscript_table)
+    def _underscore_to_subscript(self, matchobj):
+        original_text = matchobj.group(0)
+        return original_text.translate(self._subscript_table)
 
-	def _caret_to_superscript(self, matchobj):
-		original_text = matchobj.group(0)
-		return original_text.translate(self._superscript_table)
+    def _caret_to_superscript(self, matchobj):
+        original_text = matchobj.group(0)
+        return original_text.translate(self._superscript_table)
 
-	def _mbox_to_text(self, matchobj):
-		mbox_text = re.search(r'\{.+\}', matchobj.group(0))
-		return mbox_text.group(0)[1:-1]
+    def _mbox_to_text(self, matchobj):
+        mbox_text = re.search(r'\{.+\}', matchobj.group(0))
+        return mbox_text.group(0)[1:-1]
